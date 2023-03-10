35a283b7b8abebd27dfce7b14b6cc86cc5931153
Anton <dr.dud34@gmail.com>
2023-03-11 02:19:23 +0300

    feat: add alias pathes for root directories

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +7,13 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-})
+  resolve: {
+    alias: {
+      components: '/src/components',
+      consts: '/src/consts',
+      pages: '/src/pages',
+      types: '/src/types',
+      utils: '/src/utils',
+    },
+  },
+});
